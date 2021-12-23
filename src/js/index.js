490808b1bcd51c09240568127888f4bfa526924f
chuenkoD <73111895+chuenkoD@users.noreply.github.com>
2021-12-23 20:03:27 +0200

    Get api

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,24 @@
 const connection = require('./connection');
 const express = require('express');
+const models = require('./models');
 
 const app = express();
 
 app.get('/users', async (req, res) => {
-    
+    const users = await models.User.findAll();
+    res.send(users);
 });
+
+app.get('/experts', async (req, res) => {
+    const users = await models.Expert.findAll();
+    res.send(users);
+});
+
+app.get('/user/:id', async (req, res) => {
+    const user = (await models.Expert.findAll())[req.params.id];
+    res.send(user);
+});
+
 
 app.listen(3000, async () => {
     await connection.sync();
